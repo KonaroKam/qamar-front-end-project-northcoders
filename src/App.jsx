4e620e6b44f570542b4fdce74090ce08cdba6d2c
KonaroKam <qamar-usman@hotmail.com>
2022-11-04 14:11:18 +0000

    add skeleton for users login page]

--- src/App.jsx
+++ src/App.jsx
@@ -2,30 +2,44 @@ import "./CSS/App.css";
 import "./CSS/reusable.css";
 import "./CSS/Reviews.css";
 import "./CSS/Menus.css";
-import "./CSS/Comments.css"
+import "./CSS/Comments.css";
 
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
+import { UserContext } from "./Contexts/UserContext";
 
+import Login from "./components/Login";
 import Header from "./components/persistent/Header";
 import ReviewsPage from "./components/ReviewsPage";
 import SingleReview from "./components/SingleReview";
 
 function App() {
+	const [userName, setUserName] = useState();
+
 	return (
-		<div className="App">
-			<Header />
-			<Routes>
-				<Route
-					path="/"
-					element={<ReviewsPage tag="All game reviews" />}
-				/>
-				<Route
-					path="/category/:category"
-					element={<ReviewsPage tag="Only " />}
-				/>
-				<Route path="/reviews/:review_id" element={<SingleReview />} />
-			</Routes>
-		</div>
+		<UserContext.Provider value={{ userName, setUserName }}>
+			<div className="App">
+				<Header />
+				{userName ? (
+					<Routes>
+						<Route
+							path="/"
+							element={<ReviewsPage tag="All game reviews" />}
+						/>
+						<Route
+							path="/category/:category"
+							element={<ReviewsPage tag="Only " />}
+						/>
+						<Route
+							path="/reviews/:review_id"
+							element={<SingleReview />}
+						/>
+					</Routes>
+				) : (
+					<Login />
+				)}
+			</div>
+		</UserContext.Provider>
 	);
 }
 
---
+++ src/components/Login.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+
+export default function Header() {
+
+    const [users, setUsers] = useState()
+
+	return (
+		<main>
+			<h2>potatoes</h2>
+		</main>
+	);
+}
